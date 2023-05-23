06eb526570eaac672868d61ec0ec736f9da30b9a
james-fleming394 <james.fleming394@gmail.com>
2023-05-23 19:20:35 -0400

    Home page set up and connected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Navbar />
+        <div className='App-pages'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div> 
     </div>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../home/Home.css';
+
+const Home = () => {
+
+    return(
+        <div className="Home">
+            
+        </div>
+    )
+}
+
+export default Home;
