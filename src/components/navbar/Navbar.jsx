72daf0031c70d388cd16ab39293f64595d9c7bc2
james-fleming394 <james.fleming394@gmail.com>
2023-05-23 15:30:44 -0400

    Navbar content in place

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import '../navbar/Navbar.css';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
     return(
         <div className="Navbar">
-            
+            <ul className="nav">
+                <li className="-nav-li"><Link to="/">Home</Link></li>
+                <li className="-nav-li"><Link to="/services">Services</Link></li>
+                <li className="-nav-li"><Link to="/staff">Staff</Link></li>
+                <li className="-nav-li"><Link to="/gallery">Gallery</Link></li>
+                <li className="-nav-li"><Link to="/products">Products</Link></li>
+                <li className="-nav-li"><Link to="/about">About</Link></li>
+            </ul>
         </div>
     )
 }
