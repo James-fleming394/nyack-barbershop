abc52bd4b4eff180febb23b62f79e0adec763550
james-fleming394 <james.fleming394@gmail.com>
2023-05-23 15:09:58 -0400

    Navbar in place and folders organzied

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      
+      <Navbar />
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import './Header.css';
-import Logo from '../assets/logo-large.png';
+import '../header/Header.css';
+import Logo from '../../assets/logo-large.png';
 
 const Header = () => {
 
