94729aecaa61c9d699c7d2214aa70dabdb58ba48
james-fleming394 <james.fleming394@gmail.com>
2023-05-24 17:57:29 -0400

    Home page title and image in place

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import '../home/Home.css';
+import Home1 from '../../assets/Home.png';
 
 const Home = () => {
 
     return(
         <div className="Home">
-            
+            <div className="welcome">
+                <h1>Welcome to Nyack Barbershop</h1>
+                <p>Cutting Edge Styles for Every Generation</p>
+            </div>
+            <div className="image-home">
+                <img className="image" src={Home1} alt="Home" />
+            </div>
+            <div className="yelp">
+                
+            </div>
         </div>
     )
 }
