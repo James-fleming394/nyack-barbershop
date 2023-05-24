b6c24586ada2cf02197ee3dd4044afe5745078bf
james-fleming394 <james.fleming394@gmail.com>
2023-05-24 13:25:37 -0400

    Header icons in place and styled

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,15 +23,18 @@ const Header = () => {
                 <h4 className="h4"><a className="tel-link" href="tel:+18453582030">(845) 358-2030</a></h4>
             </div>
             <div className="directions">
+                <h4 className="h4">Check Us Out</h4>
                 <h6 className="h6"><a className="direction-link" 
                 href="https://www.google.com/maps?um=1&fb=1&gl=us&sa=X&geocode=Kc_LIfkN6sKJMdqIIglked4X&daddr=69+Main+St,+Nyack,+NY+10960&ved=0CBAQiBNqFwoTCKD44aPui_8CFQAAAAAdAAAAABAY">
                 69 Main St Nyack, NY 10960</a></h6>
-                <h6 className="h6"><a className="icon-link" href="https://www.facebook.com/nyackbarbershop">
-                <img className="icon-header" src={FacebookIcon} alt="icon" /></a></h6>
-                <h6 className="h6"><a className="icon-link" href="https://www.facebook.com/nyackbarbershop">
-                <img className="icon-header" src={InstagramIcon} alt="icon" /></a></h6>
-                <h6 className="h6"><a className="icon-link" href="https://www.facebook.com/nyackbarbershop">
-                <img className="icon-header" src={YelpIcon} alt="icon" /></a></h6>
+                <div className="icon-links-container">
+                    <h6 className="h6-img"><a className="icon-link" href="https://www.facebook.com/nyackbarbershop">
+                        <img className="icon-header" src={FacebookIcon} alt="icon" /></a></h6>
+                    <h6 className="h6-img"><a className="icon-link" href="https://www.instagram.com/nyackbarbershop/?hl=en">
+                        <img className="icon-header" src={InstagramIcon} alt="icon" /></a></h6>
+                    <h6 className="h6-img"><a className="icon-link" href="https://www.yelp.com/biz/nyack-barbershop-nyack">
+                        <img className="icon-header" src={YelpIcon} alt="icon" /></a></h6>
+                </div>
             </div>
         </div>
     )
