abaf99cb9961b55593e058b3c1cf2dfd5fb3f7da
james-fleming394 <james.fleming394@gmail.com>
2023-06-01 14:21:30 -0400

    Footer sign up complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import './Footer.css';
+import { useState } from "react";
 
 const Footer = () => {
 
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert('Thank you for joining our community!');
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
     return(
         <div className="footer">
-
+            <div className="footer-signup">
+                <h5>Sign up to stay up to deal with our weekly deals!</h5>
+                <form className="newsletter form">
+                    <input className="footer-input" 
+                        type="email"
+                        placeholder="Enter Your Email Here"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <button className="footer-btn" onClick={handleSubmit}>Sign Up</button>
+                </form>
+            </div>
         </div>
     )
 }
