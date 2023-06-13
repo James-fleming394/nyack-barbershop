f6e11412f52c076db3a9053142e4d0603064448d
james-fleming394 <james.fleming394@gmail.com>
2023-06-13 17:06:07 -0400

    Book Now CSS update and mobile CSS fix

--- src/pages/book/Booking.jsx
+++ src/pages/book/Booking.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import './Booking.css';
 import { useState } from "react";
+import Vlad from '../../assets/vlad-icon.png';
+import Joe from '../../assets/joe-barbershop.png';
+import Jenny from '../../assets/jenny-barbershop.png';
+import Arthur from '../../assets/arthur-barbershop.png';
+import Facebook from '../../assets/facebook.png';
+import Instagram from '../../assets/instagram.png';
+
 
 const Booking = () => {
 
@@ -23,10 +30,6 @@ const Booking = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        console.log('Selected Barber:', selectedBarber);
-        console.log('Selected Time:', selectedTime);
-        console.log('Selected Date:', selectedDate);
-
         setShowPopup(true);
     };
 
@@ -36,7 +39,7 @@ const Booking = () => {
 
     const Popup = ( {onClose} ) => {
         return (
-            <div className="popup">
+            <div className="popup-overlay">
                 <div className="popup-content">
                 <h2>Thank you for your appointment!</h2>
                 <button onClick={onClose}>Close</button>
@@ -50,10 +53,10 @@ const Booking = () => {
     return(
         <div className="booking">
             <h1 className="book-h1">Book an Appointment</h1>
-        <form onSubmit={handleSubmit}>
-        <div>
-            <label>Barber:</label>
-            <select value={selectedBarber} onChange={handleBarberChange}>
+        <form className="booking-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+            <label className="form-label">Barber:</label>
+            <select className="form-select" value={selectedBarber} onChange={handleBarberChange}>
             <option value="">Select a barber</option>
             <option value="barber1">Vlad</option>
             <option value="barber2">Joe </option>
@@ -61,18 +64,50 @@ const Booking = () => {
             <option value="barber3">Arthur</option>
             </select>
         </div>
-        <div>
-            <label>Time:</label>
-            <input type="time" value={selectedTime} onChange={handleTimeChange} />
+        <div className="form-group">
+            <label className="form-label">Time:</label>
+            <input type="time" className="form-input" value={selectedTime} onChange={handleTimeChange} />
         </div>
-        <div>
-            <label>Date:</label>
-            <input type="date" value={selectedDate} onChange={handleDateChange} />
+        <div className="form-group">
+            <label className="form-label">Date:</label>
+            <input type="date" className="form-input" value={selectedDate} onChange={handleDateChange} />
         </div>
-        <button type="submit">Book Appointment</button>
+        <button className="form-button" type="submit">Book Appointment</button>
         </form>
         {showPopup && <Popup onClose={handleClosePopup} />}
+        <h1 className="icon-h1">Our Staff</h1>
+        <div className="barber-icons">
+                <div className="barber-card">
+                    <img className="barber-card-img" src={Vlad} alt="barber card" />
+                    <h2 className="barber-icon-name">Vlad</h2>
+                    <p className="barber-icon-p">Owner <br></br> Master Barber</p>
+                    <a href="https://www.facebook.com/nyackbarbershop"><img className="social-img-icon" src={Facebook} alt="icon" /></a>
+                    <a href="https://www.instagram.com/nyackbarbershop/"><img className="social-img-icon" src={Instagram} alt="icon" /></a>
+                </div>
+                <div className="barber-card">
+                    <img className="barber-card-img" src={Arthur} alt="barber card" />
+                    <h2 className="barber-icon-name">Arthur</h2>
+                    <p className="barber-icon-p">Barber</p>
+                    <a href="https://www.facebook.com/nyackbarbershop"><img className="social-img-icon" src={Facebook} alt="icon" /></a>
+                    <a href="https://www.instagram.com/nyackbarbershop/"><img className="social-img-icon" src={Instagram} alt="icon" /></a>
+                </div>
+                <div className="barber-card">
+                    <img className="barber-card-img" src={Joe} alt="barber card" />
+                    <h2 className="barber-icon-name">Joe</h2>
+                    <p className="barber-icon-p">Master Barber</p>
+                    <a href="https://www.facebook.com/nyackbarbershop"><img className="social-img-icon" src={Facebook} alt="icon" /></a>
+                    <a href="https://www.instagram.com/nyackbarbershop/"><img className="social-img-icon" src={Instagram} alt="icon" /></a>
+                </div>
+                <div className="barber-card">
+                    <img className="barber-card-img" src={Jenny} alt="barber card" />
+                    <h2 className="barber-icon-name">Jenny</h2>
+                    <p className="barber-icon-p">Barber <br></br> Hair Stylist</p>
+                    <a href="https://www.facebook.com/nyackbarbershop"><img className="social-img-icon" src={Facebook} alt="icon" /></a>
+                    <a href="https://www.instagram.com/nyackbarbershop/"><img className="social-img-icon" src={Instagram} alt="icon" /></a>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
