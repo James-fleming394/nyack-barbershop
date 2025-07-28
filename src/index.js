2810d6e618acbd973df3c71f8c2d25b364c8d07e
james-fleming394 <james.fleming394@gmail.com>
2025-07-28 12:36:18 -0400

    Update: Fixed ResizeObserver Runtime error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// Suppress ResizeObserver warning
+window.addEventListener('error', (e) => {
+  if (e.message === 'ResizeObserver loop completed with undelivered notifications.') {
+    e.stopImmediatePropagation();
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
